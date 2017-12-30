a5cf062c69f0e07b81ca33ad4d105826a9c75cd0
Ryan Azrian <azrian.ryan@gmail.com>
2017-12-30 11:54:32 +0700

    Ryan

---
+++ Assets/Script/CarControlActive.js
@@ -0,0 +1,8 @@
+var CarControl : GameObject;
+var DreamCar01 : GameObject;
+
+
+function Start(){
+	CarControl.GetComponent("CarController").enabled = true;	
+	DreamCar01.GetComponent("CarAIControl").enabled = true;
+}
